295d544d4e27a6c7f49a1b8f3738b30e4b672d9e
Will Saunders <will.saunders@ukaea.uk>
2024-11-27 17:08:31 +0000

    prototype running from build to test run

--- build_run_tests.py
+++ build_run_tests.py
@@ -2,25 +2,28 @@ import reframe as rfm
 import reframe.utility.sanity as sn
 import os
 
+
 @rfm.simple_test
 class NESOParticlesBuild(rfm.CompileOnlyRegressionTest):
     def __init__(self):
-        self.descr = ('Build and run tests.')
-        self.valid_systems = ['NESOParticlesReframe']
-        self.valid_prog_environs = ['*']
-        self.executable = 'test/testNESOParticles'
-        self.sourcesdir = os.path.join(os.path.dirname(__file__), "..", "NESO-Particles", )
-        self.build_system = 'CMake'
+        self.descr = "Build and run tests."
+        self.valid_systems = ["NESOParticlesReframe"]
+        self.valid_prog_environs = ["*"]
+        self.executable = "test/testNESOParticles"
+        self.sourcesdir = os.path.join(
+            os.path.dirname(__file__),
+            "..",
+            "NESO-Particles",
+        )
+        self.build_system = "CMake"
 
     @run_before("compile")
     def prepare_build(self):
-        print("MARCO")
-        print(self.current_environ)
-        print(self.current_environ.modules)
-        print("POLO")
         cmake_flags = self.current_environ.extras["cmake_configuration"]
         self.build_system.config_opts = cmake_flags
-        self.build_system.max_concurrency = self.current_environ.extras["num_build_workers"]
+        self.build_system.max_concurrency = self.current_environ.extras[
+            "num_build_workers"
+        ]
         self.build_system.make_opts += ["testNESOParticles"]
 
     @sanity_function
@@ -30,21 +33,22 @@ class NESOParticlesBuild(rfm.CompileOnlyRegressionTest):
 
 @rfm.simple_test
 class NESOParticlesTest(rfm.RunOnlyRegressionTest):
-    # test_binaries = fixture(NESOParticlesBuild, scope='environment')
-    valid_systems = ['NESOParticlesReframe']
-    valid_prog_environs = ['*']
+    test_binaries = fixture(NESOParticlesBuild, scope="environment")
+    valid_systems = ["NESOParticlesReframe"]
+    valid_prog_environs = ["*"]
 
-    @run_before('run')
+    @run_before("run")
     def setup_omp_env(self):
-        #self.executable = os.path.join(self.test_binaries.stagedir, 'test','testNESOParticles')
-        self.executable = "/home/js0259/git-ukaea/NESO-Particles-reframe/stage/NESO-Particles-reframe/default/acpp_omp_library_only/NESOParticlesBuild/test/testNESOParticles"
+        self.executable = os.path.join(
+            self.test_binaries.stagedir, "test", "testNESOParticles"
+        )
         procinfo = self.current_partition.processor
         self.num_tasks = self.current_partition.max_jobs
         self.num_cpus_per_task = procinfo.num_cores
         self.env_vars = {
-            'OMP_NUM_THREADS': 2,
+            "OMP_NUM_THREADS": 2,
         }
 
     @sanity_function
     def validate_solution(self):
-        return True
+        return sn.assert_not_found(r"FAILED", self.stdout)
--- config/baseline.py
+++ config/baseline.py
@@ -1,22 +1,23 @@
 site_configuration = {
-    'systems': [
+    "systems": [
         {
-            'name': 'NESOParticlesReframe',
-            'descr': 'Runs NESO-Particles tests.',
-            'hostnames': ['L0211-XU'],
-            'modules_system': 'tmod4',
-            'partitions': [
+            "name": "NESOParticlesReframe",
+            "descr": "Runs NESO-Particles tests.",
+            "hostnames": ["L0211-XU"],
+            "modules_system": "tmod4",
+            "partitions": [
                 {
                     "max_jobs": 12,
-                    'name': 'default',
-                    'descr': 'Example partition',
-                    'scheduler': 'local',
-                    'launcher': 'mpirun',
-                    'environs': ['acpp_omp_library_only'],
-                    'prepare_cmds': ["source /usr/local/Modules/init/profile.sh"]
+                    "name": "default",
+                    "descr": "Example partition",
+                    "scheduler": "local",
+                    "launcher": "mpirun",
+                    "environs": ["acpp_omp_library_only"],
+                    "prepare_cmds": [
+                        "source /usr/local/Modules/init/profile.sh"
+                    ],
                 }
-            ]
-
+            ],
         }
     ],
     "environments": [
@@ -28,12 +29,14 @@ site_configuration = {
             "features": [],
             "extras": {
                 "cmake_configuration": [
-                    "-DACPP_TARGETS=omp.library-only", 
-                    "-DCMAKE_BUILD_TYPE=Debug"
+                    "-DACPP_TARGETS=omp.library-only",
+                    "-DCMAKE_BUILD_TYPE=Debug",
                 ],
-                "num_build_workers" : 12
+                "num_build_workers": 12,
             },
-            "modules": ["reframe/NP-acpp-llvm-cuda",]
+            "modules": [
+                "reframe/NP-acpp-llvm-cuda",
+            ],
         },
-    ]
+    ],
 }
